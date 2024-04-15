cb45dbdfd0ec01fcd2eea5adb46995b873756b4a
laila.alhasan <laila.alhasan@hva.nl>
2024-04-15 16:52:57 +0200

    Add UserAddress entity for storing user address information.

---
+++ foryou-server/src/main/java/nl/hva/foryou/presistence/domain/UserAddress.java
@@ -0,0 +1,68 @@
+package nl.hva.foryou.presistence.domain;
+
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToOne;
+
+public class UserAddress extends BaseJpaEntity {
+
+    @OneToOne(optional = false, fetch = FetchType.LAZY)
+    private User user;
+
+    private String street;
+
+    private String houseNumber;
+
+    private String houseNumberSuffix;
+
+    private String postalCode;
+
+    private String city;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getHouseNumberSuffix() {
+        return houseNumberSuffix;
+    }
+
+    public void setHouseNumberSuffix(String houseNumberSuffix) {
+        this.houseNumberSuffix = houseNumberSuffix;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
