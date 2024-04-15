a2346272f9a62c4eff90a70626ffa8468838a423
Yasin <yasin.el.bouazzati@ad.hva.nl>
2024-04-15 12:30:56 +0200

    Initial commit of back-end

---
+++ foryou-server/src/main/java/nl/hva/foryou/ForyouApplication.java
@@ -0,0 +1,13 @@
+package nl.hva.foryou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForyouApplication.class, args);
+	}
+
+}
---
+++ foryou-server/src/test/java/nl/hva/foryou/ForyouApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hva.foryou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
