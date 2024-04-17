c4f7965b6eeb7cd1b5f03d90b17800813c9eb103
Mohaned Al-Jabori <mohaned.al-jabori@hva.nl>
2024-04-17 18:01:15 +0200

    Navbar created and implemented

--- foryou-frontend/src/pages/register/register.tsx
+++ foryou-frontend/src/pages/register/register.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import css from './register.module.css';
 import {Shapes} from "../../components/background-shapes/shapes";
 import {RegisterCard} from "../../components/register-card/register-card";
+import Navbar from "../../components/navbar/navbar";
 
 export const Register: React.FC = () => {
     return (
         <div className={css.container}>
+            <Navbar/>
             <Shapes/>
             <RegisterCard/>
         </div>
