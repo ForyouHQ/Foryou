c6c6cfd3b8a3c1c7870541157684895eb7047ab7
Yasin <yasin.el.bouazzati@ad.hva.nl>
2024-04-22 18:08:17 +0200

    (Ticket #3) Code format and implemented dashboard component file.

---
+++ foryou-frontend/src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import css from './dashboard.module.css';
+
+export const Dashboard: React.FC = () => {
+    return (
+        <div className={css.container}>
+
+        </div>
+    );
+};
