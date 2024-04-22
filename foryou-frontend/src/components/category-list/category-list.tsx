e0d81961ebd6471e3e89821db397dd899a704627
Yasin <yasin.el.bouazzati@ad.hva.nl>
2024-04-22 20:55:52 +0200

    (Ticket #21) Add category list component.

---
+++ foryou-frontend/src/components/category-list/category-list.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import css from './category-list.module.css';
+
+export const CategoryList: React.FC = () => {
+    return (
+        <div className={css.container}>
+
+        </div>
+    );
+};
