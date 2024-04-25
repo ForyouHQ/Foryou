189bf514cf37cf125d9b842f7a9b726956091c30
Mohaned <mohaned.al-jabori@hva.nl>
2024-04-25 22:58:51 +0200

    removed crossorigin annotation

--- foryou-server/src/main/java/nl/hva/foryou/api/controller/UserController.java
+++ foryou-server/src/main/java/nl/hva/foryou/api/controller/UserController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/auth", produces = MediaTypes.HAL_JSON_VALUE)
-@CrossOrigin(origins = "*")
 public class UserController {
 
     private final UserService userService;
