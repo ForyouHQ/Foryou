3b9e3bfce375cd3639c1b4eddad18c76faa21ae6
Yasin <yasin.el.bouazzati@ad.hva.nl>
2024-04-30 23:35:39 +0200

    ci/cd

---
+++ foryou-server/src/main/java/nl/hva/foryou/api/auth/GlobalConfig.java
@@ -0,0 +1,20 @@
+package nl.hva.foryou.api.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class GlobalConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true);
+    }
+}
