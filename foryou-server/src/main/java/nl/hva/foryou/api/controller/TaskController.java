7d9d4920c88750092af94813c06dcaa9705bef64
Mohaned <mohaned.al-jabori@hva.nl>
2024-05-09 01:01:51 +0200

    cors update

--- foryou-server/src/main/java/nl/hva/foryou/api/controller/TaskController.java
+++ foryou-server/src/main/java/nl/hva/foryou/api/controller/TaskController.java
@@ -32,7 +32,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/tasks", produces = MediaTypes.HAL_JSON_VALUE)
-@CrossOrigin(origins = "https://foryou-frontend-test.onrender.com")
+@CrossOrigin(origins = "*")
 public class TaskController {
 
     private final TaskService taskService;
